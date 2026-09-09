c68c887e1819d0c6db7bb85b258f63758379429d
mamal41 <mohammadreza.masjedi@gmail.com>
2026-09-09 10:21:57 +0200

    Update main.dart
    Ckaude

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(const MoneyApp());
